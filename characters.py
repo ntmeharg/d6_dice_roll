5fd0991892b999eceee6dd38b1eedd5339695653
Nate Meharg <ntmeharg@gmail.com>
2022-05-06 14:37:36 -0500

    add character file

---
+++ characters.py
@@ -0,0 +1,22 @@
+#TODO: Create a dictionary of my character
+
+#skill dictionaries
+dexterity = {"Default": 4}
+perception = {"Default": 4}
+knowledge = {"Default": 4}
+strength = {"Default": 4}
+mechanical = {"Default": 4}
+technical = {"Default": 4}
+
+#Attributes Dictionary
+attributes = {
+    "Dexterity": {dexterity},
+    "Perception": {perception},
+    "Knowledge": {knowledge},
+    "Strength": {strength},
+    "Mechanical": {mechanical},
+    "Technical": {technical}
+}
+
+print(attributes)
+
